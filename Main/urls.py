4390b99c456d045cc5a92690b45eb7bbc82d0342
dlwnsgud14 <dlwnsgud14@naver.com>
2017-08-12 20:18:39 +0900

    2017-08-05

--- Main/urls.py
+++ Main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'Main'
 urlpatterns = [
     #캐럿은 빈경로를 의미
-    url(r'^$', views.index),
+    url(r'^$', views.index, name = 'Friends Home'),
     url(r'^house/detail/(?P<id>\d+)/$', views.detail)
     #url(r'^house/detail/(?P<poll_id>\d+)/$', views.detail),
 
